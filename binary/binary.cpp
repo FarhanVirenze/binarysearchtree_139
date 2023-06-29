4fb6c641841032366c27ff26f43ee9e7f6a0a970
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-29 15:57:28 +0700

    tambahan fungsi while looping (true)

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -123,7 +123,10 @@ public:
 int main()
 {
 	BinaryTree obj;
-	
+	while (true)
+	{
+
+	}
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
