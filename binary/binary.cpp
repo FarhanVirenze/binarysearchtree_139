a51a0847139a6964e18ac1abb14f70a8456ef584
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-29 15:12:22 +0700

    tambahan fungsi class Node

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -2,11 +2,13 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+class Node
 {
-    std::cout << "Hello World!\n";
-}
+
+};
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
