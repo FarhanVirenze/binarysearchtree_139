10a9508cf82b4a95b65e12f7e869074d154d7891
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-29 15:15:21 +0700

    tambahan fungsi class BinaryTree

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -7,6 +7,22 @@ using namespace std;
 
 class Node
 {
+public:
+	string info;
+		Node* leftchild;
+		Node* rightchild;
+
+		// Constructor for the node class
+		Node(string i, Node* l, Node* r)
+		{
+			info = i;
+			leftchild = l;
+			rightchild = r;
+		}
+};
+
+class BinaryTree
+{
 
 };
 
