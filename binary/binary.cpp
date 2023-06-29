78d5a8237c8c253b2a47a608894fb6e4055d5abd
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-29 15:40:52 +0700

    tambahan fungsi while looping ((currentNode != NULL) && (currentNode->info != element))

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -60,7 +60,13 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		// This function searches the currentNodeof the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
 
+		}
 	}
 };
 
